2a5a9f7ab009d51cc5766811ba8fdf14a1399056
Roman <vojtus.roman@gmail.com>
2022-12-20 22:45:05 +0100

    ..

--- Open-Lab-05.01/DateTools.cs
+++ Open-Lab-05.01/DateTools.cs
@@ -6,7 +6,32 @@ namespace Open_Lab_05._01
     {
         public string Century(int year)
         {
-            throw new NotImplementedException();
+            double centuryint = year / 100;
+            centuryint = Math.Ceiling((double)year / 100);
+            int lastdigit = (Convert.ToInt32(centuryint) % 10);
+            if (lastdigit == 1 && centuryint != 11)
+            {
+                string result = centuryint.ToString() + "st century";
+                return result;
+            }
+            else if (lastdigit == 2 && centuryint != 12)
+            {
+                string result = centuryint.ToString() + "nd century";
+                return result;
+
+            }
+            else if (lastdigit == 3 && centuryint != 13)
+            {
+                string result = centuryint.ToString() + "rd century";
+                return result;
+            }
+            else
+            {
+                string result = centuryint.ToString() + "th century";
+                return result;
+            }
+                
+
         }
     }
 }
